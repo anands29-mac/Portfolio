cca739842e5804b309eda964bc70976621f0c88f
anands29 <anands29@mcmaster.ca>
2024-10-18 00:30:06 -0400

    deploy

---
+++ index.js
@@ -0,0 +1,11 @@
+// Script to handle download resume button
+// This script can be used for adding additional JavaScript functionality.
+// Example: Smooth scrolling for internal navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
